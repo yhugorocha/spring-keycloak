6fd464b9025d77e71cf95b33f4108142b50fb899
Hugo Silva <jose.silva@extreme.digital>
2024-09-26 09:07:15 -0300

    initial commit

---
+++ src/main/java/io/github/yhugorocha/SpringKeyCloak/SpringKeyCloakApplication.java
@@ -0,0 +1,13 @@
+package io.github.yhugorocha.SpringKeyCloak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKeyCloakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKeyCloakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/yhugorocha/SpringKeyCloak/SpringKeyCloakApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.yhugorocha.SpringKeyCloak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKeyCloakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
